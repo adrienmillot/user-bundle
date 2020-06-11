78a5f411d857c9e91a8e6ec1e55c02eb3e07c443
Adrien <adrien.millot@gmail.com>
2020-06-11 16:24:02 +0200

    Add Profile extension

--- Twig/UserExtension.php
+++ Twig/UserExtension.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace amillot\UserBundle\Twig;
 
+use amillot\UserBundle\Model\UserInterface;
 use Doctrine\Common\Persistence\ManagerRegistry;
 use Knp\Component\Pager\PaginatorInterface;
 use Twig\Environment;
@@ -20,8 +21,11 @@ class UserExtension extends AbstractExtension
 
     private $registry;
 
-    public function __construct($prmUserClassName, Environment $prmEnvironment, ManagerRegistry $prmRegistry, PaginatorInterface $prmPager)
-    {
+    public function __construct($prmUserClassName,
+                                Environment $prmEnvironment,
+                                ManagerRegistry $prmRegistry,
+                                PaginatorInterface $prmPager
+    ) {
         $this->userClassName = $prmUserClassName;
         $this->environment = $prmEnvironment;
         $this->paginator = $prmPager;
@@ -31,8 +35,18 @@ class UserExtension extends AbstractExtension
     public function getFunctions()
     {
         return [
+            new TwigFunction('render_profile', [$this, 'renderProfile'], ['is_safe' => ['html']]),
             new TwigFunction('render_user_list', [$this, 'renderUserList'], ['is_safe' => ['html']]),
         ];
+    }
+
+    public function renderProfile(UserInterface $prmUser)
+    {
+        $profile = $prmUser->getProfile();
+
+        return $this->environment->render('@User/twig/profile/_show.html.twig', [
+            'profile' => $profile,
+        ]);
     }
 
     public function renderUserList(int $prmOffset, int $prmLimit = 15)
